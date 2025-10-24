5dd2300222205bee9fae59da54c835d5d978aeea
Ahn Kiwook <ahnkiwook@gmail.com>
2025-10-24 23:56:19 +0900

    Use channel thumbnail properly

--- app/youtube/useVideoInfo.ts
+++ app/youtube/useVideoInfo.ts
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { VideoInfo } from './types';
 import useInnertube from './useInnertube';
 
 export default function useVideoInfo(videoId: string | undefined) {
   const [videoInfo, setVideoInfo] = useState<VideoInfo | null>(null);
+  const thumbnailUrlRef = useRef<string | null>(null);
 
   const { innertube } = useInnertube();
 
@@ -13,22 +14,32 @@ export default function useVideoInfo(videoId: string | undefined) {
     }
 
     const fn = async () => {
-      await innertube.getInfo(videoId).then((info) =>
-        setVideoInfo({
-          videoId: info.basic_info.id,
-          title: info.basic_info.title,
-          channelName: info.basic_info.author,
-          channelId: info.basic_info.channel_id,
-          isLive: info.basic_info.is_live || false,
-          viewerCount: undefined,
-          thumbnailUrl: info.basic_info.thumbnail?.[0]?.url,
-        }),
-      );
+      const info = await innertube.getInfo(videoId);
+      const channelId = info.basic_info.channel_id;
+      if (thumbnailUrlRef.current == null) {
+        const channel = await innertube.getChannel(channelId);
+        thumbnailUrlRef.current = channel.metadata.thumbnail[0].url;
+      }
+      setVideoInfo({
+        videoId: info.basic_info.id,
+        title: info.basic_info.title,
+        channelName: info.basic_info.author,
+        channelId,
+        isLive: info.basic_info.is_live || false,
+        viewerCount: undefined,
+        thumbnailUrl: thumbnailUrlRef.current,
+      });
     };
     void fn();
     const interval = setInterval(() => void fn(), 30000);
     return () => clearInterval(interval);
   }, [innertube, videoId]);
+
+  useEffect(() => {
+    if (videoId != null) {
+      thumbnailUrlRef.current = null;
+    }
+  }, [videoId]);
 
   return { videoInfo };
 }
